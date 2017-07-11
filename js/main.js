98c826079b5841d45df1680bb27c95fa295d57a6
Roy Chemo <roych28@gmail.com>
2017-07-11 18:20:41 +0300

    random lines on sphere with animation info text

--- js/main.js
+++ js/main.js
@@ -11,15 +11,47 @@ orbitCamera.minDistance = 1;
 camera.position.set(0,0,3);
 camera.lookAt(new THREE.Vector3(0,0,0));
 
-var cloudMesh = null;
+window.addEventListener('resize', function() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
 
-//init renderer
+
+var cloudMesh = null;
+var drawLines = false;
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000);
 document.body.appendChild(renderer.domElement);
 
 var earthObject = new THREE.Object3D();
 scene.add(earthObject);
+
+var linesObject = new THREE.Object3D();
+linesObject.scale.set(0.5,0.5,0.5);
+scene.add(linesObject);
+
+var createHud = function(){
+
+  var button = document.createElement( 'div' );
+  button.id = 'button';
+  button.textContent = 'ShowLines';
+  button.addEventListener( 'click', function ( event ) {
+
+    drawLines = !drawLines;
+    button.textContent = drawLines ? 'HideLines' : 'ShowLines';
+
+  }, false );
+
+  document.body.appendChild( button );
+
+  var text = document.createElement( 'div' );
+  text.id = 'info';
+  text.textContent = 'Drag - rotate around earth Scroll - zoom';
+
+  document.body.appendChild( text );
+}
 
 var createLights = function(){
   var light	= new THREE.DirectionalLight( 0xaaaaaa, 1.0 );
@@ -51,8 +83,41 @@ var createEarthCloud	= function(){
   return mesh
 }
 
+function randomSpherePoint(x0,y0,z0,radius){
+  var u = Math.random();
+  var v = Math.random();
+  var theta = 2 * Math.PI * u;
+  var phi = Math.acos(2 * v - 1);
+  var x = x0 + (radius * Math.sin(phi) * Math.cos(theta));
+  var y = y0 + (radius * Math.sin(phi) * Math.sin(theta));
+  var z = z0 + (radius * Math.cos(phi));
+  return [x,y,z];
+}
+
+var createLines = function(){
+  var material = new THREE.LineBasicMaterial({
+    color: 0x0000ff
+  });
+
+  for(var i = 0; i< 30; i++){
+      var geometry = new THREE.Geometry();
+      var pos = randomSpherePoint(0,0,0,0.7);
+      geometry.vertices.push(
+        new THREE.Vector3( 0, 0, 0 ),
+        new THREE.Vector3( pos[0], pos[1], pos[2] )
+      );
+
+      var line = new THREE.Line( geometry, material );
+      linesObject.add( line );
+  }
+
+  earthObject.add(linesObject);
+
+}
+
 var createScene = function(){
 
+  createHud();
   createLights();
   createSpace();
 
@@ -70,9 +135,9 @@ var createScene = function(){
 
   cloudMesh = createEarthCloud();
   earthObject.add(cloudMesh);
-}
 
-createScene();
+  createLines();
+}
 
 function render(){
   requestAnimationFrame(render);
@@ -84,8 +149,17 @@ function render(){
     cloudMesh.rotation.y += ((0.1*Math.PI / 180)) % 360;
   }
 
+  if( drawLines && linesObject.scale.y < 1 ){
+    var newScale = linesObject.scale.y + 0.02;
+    linesObject.scale.set(newScale,newScale,newScale);
+  }else if( !drawLines && linesObject.scale.y > 0.5 ){
+    var newScale = linesObject.scale.y - 0.02;
+    linesObject.scale.set(newScale,newScale,newScale);
+  }
+
   renderer.render(scene, camera);
 }
 
+createScene();
 requestAnimationFrame(render);
 
